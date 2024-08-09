62a23cb8c42d30c551b09c37301c64cbaa582074
Oakamoore <emmanuelogie550@yahoo.com>
2024-08-09 12:40:20 +0100

    Draw `Word` objects to a `Session` canvas

--- include/session.h
+++ include/session.h
@@ -2,43 +2,36 @@
 #define KEYWORDS_SESSION_H
 
 #include "input_component.h"
+#include "word.h"
+#include "timer.h"
 #include <ftxui/dom/node.hpp>
 #include <ftxui/component/component.hpp>
+#include <vector>
 
 namespace Keywords
 {
 	class Session
 	{
 	public:
-		Session() = default;
+		Session(/*SessionConfig config*/);
 
-		// Draw 'm_words' to 'ftxui::Canvas'
-		// Draw the stats of the 'Session' to 'ftxui::Canvas'
 		ftxui::Element draw();
 
 		ftxui::Component getInputComponent() { return m_input.getComponent(); }
 		
 	private:
-		// Called in 'Session::draw()'
-		// Reposition elements of 'm_words', in 'ftxui::Canvas'
-		// This calls 'Session::addWords()' and 'Session::eraseWords()'
 		void update();
-		
-		// Add new 'Word' objects to 'm_words'
 		void addWords();
-		
-		// When 'ENTER' is pressed this checks against
-		// 'm_words' and removes any 'Word' that matches
 		void eraseWords();
 
 	private:
 		/*const SessionConfig m_config*/ 
-		/*std::vector<Word> m_words*/
-		/*Timer m_uptime*/
-		
-		InputComponent m_input;
+		Words m_words {}; 
+		Timer m_uptime {};
+		InputComponent m_input {};
 		int m_misses {};
 		int m_charsTyped {};
+		int m_wordsTyped {};
 		int m_wordsPerMinute {};
 		double m_charsPerSecond {};
 
--- src/session.cpp
+++ src/session.cpp
@@ -1,28 +1,36 @@
 #include "session.h"
+#include "word.h"
+#include "random.h"
 #include <ftxui/dom/canvas.hpp>
 #include <ftxui/dom/elements.hpp>
+#include <array>
 
 namespace
 {
 	constexpr int g_canvasWidth {240};
 	constexpr int g_canvasHeight {100};
-
-	/*constexpr int g_maxMisses {10};*/
-
-	/*int calcuateWordsPerMinute()*/
-	/*int calcuateCharsPerSecond()*/
 }
 
 namespace Keywords
 {
+	Session::Session(/*SessionConfig config*/)
+	{
+	}
+
 	ftxui::Element Session::draw()
 	{
 		// Create a temporary blank 'ftxui::Canvas' 
 		ftxui::Canvas c {g_canvasWidth, g_canvasHeight};
 
+		c.DrawText(0, 0, "Time: " + std::to_string(m_uptime.elapsed()), ftxui::Color::White);
+		c.DrawText(0, 5, "Number of Words: " +std::to_string(m_words.size()), ftxui::Color::White);
+
 		// TODO: Loop through 'm_words' and draw each to the 'ftxui::Canvas'
+		for (const auto& word : m_words)
+			c.DrawText(word->getX(), word->getY(), word->getText(), word->getColor());
 
 		// TODO: Call 'Session::update()' post draw phase
+		update();
 
 		return ftxui::vbox({
 			ftxui::text("KEYWORDS") | ftxui::center, 
@@ -35,25 +43,49 @@ namespace Keywords
 
 	void Session::update()
 	{
+		static auto timeStamp {m_uptime.elapsed()};
+		static constexpr Timer::Second spawnDelay {5};
+
+		// Continue while 'm_misses' < 'g_maxMisses' or a 'SessionConfig' defined max
+		
+
 		// Loops through 'm_words', and increments their positions
-		// Calls 'addWords()' and 'eraseWords()' (the latter only on 'ENTER')
+		for (auto& word : m_words)
+			word->move(g_canvasWidth);
 
-
+		// Calls 'addWords()' and 'eraseWords()' 
+		
+		// A given amount of time has passed 
+		if ((m_uptime.elapsed() - timeStamp) >= spawnDelay.count())
+		{
+			addWords();
+			timeStamp = m_uptime.elapsed();
+		}
 	}
 
 	void Session::addWords()
 	{
-		// Reads in from 'WordBank', then creates new 'Word' object
+		static constexpr int spawnCount {5};
+
+		for (int i {0}; i < spawnCount; ++i)
+			m_words.emplace_back(std::make_unique<Word>("test", Random::get(10, g_canvasHeight - 1)));
+
+		// Randomly reads in from 'WordBank', then creates new 'Word' object
 		// Adds a given amount of 'Word' objects to 'm_words'
-		// Ensure all the height values are different
-
-
+		// Randomly assigns height values, ensuring they are all different
+			// Check that the position being drawn to is empty
+		// Take into account the word offset starting positions
 	}
 
 	void Session::eraseWords()
 	{
-		// Loops through 'm_words' and checks against 'Word::m_content'
-		// if they match 'InputComponent::getContent()', they are erased
+		// When 'ENTER' is pressed, loops through 'm_words' 
+		// and checks against 'Word::m_content' if they match 
+		// 'm_input::getContent()', they are erased
+
+		// Also removes any 'Word' that is out of bounds, incrementing 'm_misses'
+		// 'Word::getX() >= g_canvasWidth'
+
 
 
 	}
