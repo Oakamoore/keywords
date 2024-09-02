f4eb2d7dada67ea46bfb57e1348701028a10f9b3
Oakamoore <emmanuelogie550@yahoo.com>
2024-09-02 17:34:30 +0100

    Update `InputComponent` tests
    The updated tests reflect the fact that space characters can't be entered into an input component.

--- tests/input_component_tests.cpp
+++ tests/input_component_tests.cpp
@@ -16,10 +16,10 @@ TEST_CASE("Disallow certain input events")
 		for (char c {'A'}; c <= 'Z'; ++c)
 		{
 			// Uppercase character events should be caught 
-			REQUIRE(inputComponent.getComponent()->OnEvent(ftxui::Event::Character(c)) == true);
+			REQUIRE(inputComponent.component->OnEvent(ftxui::Event::Character(c)) == true);
 
 			// The input component should remain unchanged 
-			REQUIRE(inputComponent.getContent().empty());
+			REQUIRE(inputComponent.content.empty());
 		}
 	}
 
@@ -28,10 +28,10 @@ TEST_CASE("Disallow certain input events")
 		for (int i {0}; i <= 9; ++i)
 		{
 			// Digit character events should be caught
-			REQUIRE(inputComponent.getComponent()->OnEvent(ftxui::Event::Character(std::to_string(i))) == true);
+			REQUIRE(inputComponent.component->OnEvent(ftxui::Event::Character(std::to_string(i))) == true);
 
 			// The input component should remain unchanged
-			REQUIRE(inputComponent.getContent().empty());
+			REQUIRE(inputComponent.content.empty());
 		}
 	}
 
@@ -40,14 +40,14 @@ TEST_CASE("Disallow certain input events")
 		// Loop through the list of printable ASCII chars
 		for (char c {' '}; c <= '~'; ++c)
 		{
-			// Not a letter, digit, or space character
-			if (!std::isalpha(c) && !std::isdigit(c) && c != ' ')
+			// Ignore letters and digits 
+			if (!std::isalpha(c) && !std::isdigit(c))
 			{
 				// Special character events should be caught
-				REQUIRE(inputComponent.getComponent()->OnEvent(ftxui::Event::Character(c)) == true);
+				REQUIRE(inputComponent.component->OnEvent(ftxui::Event::Character(c)) == true);
 
 				// The input component should remain unchanged
-				REQUIRE(inputComponent.getContent().empty());
+				REQUIRE(inputComponent.content.empty());
 			}
 		}
 	}
@@ -60,15 +60,15 @@ TEST_CASE("Detect enter keypress")
 	// The component should be drawn without crashing
 	inputComponent.draw();
 
-	inputComponent.getComponent()->OnEvent(ftxui::Event::Return);
+	inputComponent.component->OnEvent(ftxui::Event::Return);
 
 	// Following an 'ENTER' event, the enter key flag should be true
-	REQUIRE(inputComponent.hasPressedEnter());
+	REQUIRE(inputComponent.hasPressedEnter);
 
-	inputComponent.getComponent()->OnEvent(ftxui::Event::Custom);
+	inputComponent.component->OnEvent(ftxui::Event::Custom);
 
 	// Following a non 'ENTER' event, the enter key flag should be false
-	REQUIRE_FALSE(inputComponent.hasPressedEnter());
+	REQUIRE_FALSE(inputComponent.hasPressedEnter);
 }
 
 TEST_CASE("Respond appropriately to a word deletion event")
@@ -81,66 +81,21 @@ TEST_CASE("Respond appropriately to a word deletion event")
 	// Event tied to word deletion
 	const auto ctrlW {ftxui::Event::Special("\x17")};
 
-	const std::string blankSpaces {"         "};
-
 	SECTION("An empty input component")
 	{
-		inputComponent.getComponent()->OnEvent(ctrlW);
+		inputComponent.component->OnEvent(ctrlW);
 
 		// A deletion event shouldn't affect an empty input component
-		REQUIRE(inputComponent.getContent().empty());
-	}
-
-	SECTION("An input component only containing blank spaces")
-	{
-		// Fill the component with blank spaces
-		inputComponent.setContent(blankSpaces);
-
-		inputComponent.getComponent()->OnEvent(ctrlW);
-
-		// All of the blank spaces should be deleted
-		REQUIRE(inputComponent.getContent().empty());
-	}
-
-	SECTION("An input component containing blank spaces followed by a word")
-	{
-		inputComponent.setContent(blankSpaces + "lorem");
-
-		inputComponent.getComponent()->OnEvent(ctrlW);
-
-		// The word should be deleted, leaving the blank spaces unchanged
-		REQUIRE(inputComponent.getContent() == blankSpaces);
-	}
-
-	SECTION("An input component containing a word followed by blank spaces")
-	{
-		inputComponent.setContent("lorem" + blankSpaces);
-
-		inputComponent.getComponent()->OnEvent(ctrlW);
-
-		// The entire contents of the input component should be deleted
-		REQUIRE(inputComponent.getContent().empty());
+		REQUIRE(inputComponent.content.empty());
 	}
 
 	SECTION("An input component containing a single word")
 	{
-		inputComponent.setContent("lorem");
+		inputComponent.content = "lorem";
 
-		inputComponent.getComponent()->OnEvent(ctrlW);
+		inputComponent.component->OnEvent(ctrlW);
 
 		// The word should be deleted, rendering the input component empty
-		REQUIRE(inputComponent.getContent().empty());
-	}
-
-	SECTION("An input component containing several space separated words")
-	{
-		inputComponent.setContent("lorem ipsum dolor sit amet");
-
-		// Two word deletion events
-		inputComponent.getComponent()->OnEvent(ctrlW);
-		inputComponent.getComponent()->OnEvent(ctrlW);
-
-		// The last two words should be deleted, and the final space should be retained
-		REQUIRE(inputComponent.getContent() == "lorem ipsum dolor ");
+		REQUIRE(inputComponent.content.empty());
 	}
 }
