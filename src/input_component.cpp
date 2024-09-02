def2a0fd3e789ba8dbc473685c78574e80647cc5
Oakamoore <emmanuelogie550@yahoo.com>
2024-09-02 17:25:52 +0100

    Disallow `SPACE` characters in an `InputComponent`

--- src/input_component.cpp
+++ src/input_component.cpp
@@ -3,31 +3,6 @@
 #include <ftxui/component/event.hpp>
 #include <cctype>
 
-namespace
-{
-	void deleteMostRecentWord(std::string& str)
-	{
-		if (!str.empty())
-		{
-			// Remove trailing spaces
-			str = str.substr(0, str.find_last_not_of(' ') + 1);
-
-			// A single word is remaining
-			if (!str.empty() && str.find(' ') == std::string::npos)
-			{
-				str.clear();
-				return;
-			}
-		
-			auto pos {str.find_last_of(' ') + 1};
-
-			// Erase backwards up until a space character
-			if (pos != std::string::npos)
-				str.erase(pos, str.length() - pos);
-		}
-	}
-}
-
 namespace Keywords
 {
 	InputComponent::InputComponent(std::string_view placeholder)
@@ -62,9 +37,8 @@ namespace Keywords
 		// Filter out digit, uppercase, and non-letter characters
 		component |= ftxui::CatchEvent([&] (ftxui::Event event)
 		{
-			return event.is_character() && std::isupper(event.character()[0])
-				|| event.is_character() && !std::isalpha(event.character()[0]) 
-				&& event.character()[0] != ' ';
+			return event.is_character() && std::isupper(event.character()[0]) || 
+				   event.is_character() && !std::isalpha(event.character()[0]);
 		});
 
 		// Register when the 'ENTER' key is pressed
@@ -75,14 +49,14 @@ namespace Keywords
 			return hasPressedEnter;
 		});
 
-		// Bind 'CTRL + W' to the deletion of the last typed word
+		// Bind 'CTRL + W' to word deletion
 		component |= ftxui::CatchEvent([&] (ftxui::Event event)
 		{
 			const auto ctrlW {ftxui::Event::Special("\x17")};
 			
 			if (event == ctrlW)
 			{
-				deleteMostRecentWord(content);
+				reset();
 				return true;
 			}
 
