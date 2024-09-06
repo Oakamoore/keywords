ccb4e946c04d09f3e7c7d1445463f06933ff1760
Oakamoore <emmanuelogie550@yahoo.com>
2024-09-06 20:19:17 +0100

    Format main menu description

--- src/main_menu.cpp
+++ src/main_menu.cpp
@@ -1,30 +1,37 @@
 #include "main_menu.h"
 #include <ftxui/component/component.hpp>
 #include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/color.hpp>
 #include <array>
+#include <string>
 #include <string_view>
 #include <optional>
 
 namespace
 {
-	using namespace std::string_view_literals;
-
-	constexpr std::array g_difficultyOptions
-	{
-		"easy"sv,
-		"medium"sv,
-		"hard"sv
-	};
-
 	std::optional<Keywords::SessionConfig::Difficulty> getDifficultyFromString(std::string_view str)
 	{
-		for (std::size_t i {0}; i < g_difficultyOptions.size(); ++i)
+		using namespace std::string_view_literals;
+		
+		static constexpr std::array difficultyOptions {"easy"sv, "medium"sv, "hard"sv};
+
+		for (std::size_t i {0}; i < difficultyOptions.size(); ++i)
 		{
-			if (str == g_difficultyOptions[i])
+			if (str == difficultyOptions[i])
 				return static_cast<Keywords::SessionConfig::Difficulty>(i);
 		}
 
 		return {};
+	}
+
+	ftxui::Element getAdjacentText(const auto& ...e)
+	{
+		return ftxui::hbox({e...}) | ftxui::center;
+	}
+
+	ftxui::Element getColoredText(const std::string& str, const ftxui::Color& color = ftxui::Color::Yellow)
+	{
+		return ftxui::text(str) | ftxui::color(color) | ftxui::center;
 	}
 
 	ftxui::Element getDescription()
@@ -33,15 +40,20 @@ namespace
 		{
 			ftxui::vbox
 			({
-				ftxui::filler(), 
-				ftxui::paragraphAlignCenter("To begin a game session type \"play\""),
-				ftxui::paragraphAlignCenter("then press ENTER."),
+				getAdjacentText(ftxui::text("To begin a game session type "), getColoredText("play"), ftxui::text(".")),
 				ftxui::separatorEmpty(),
 				ftxui::separatorEmpty(),
-				ftxui::paragraphAlignCenter("To change difficulty type either"),
-				ftxui::paragraphAlignCenter("\"easy\", \"medium\" or \"hard\""),
-				ftxui::paragraphAlignCenter("then press ENTER."),
-				ftxui::filler(),
+				ftxui::paragraphAlignCenter("To change the difficulty of a game session (in the main menu), type either:"),
+				getAdjacentText(getColoredText("easy"), ftxui::text(" (<=5 character words)")),
+				getAdjacentText(getColoredText("medium"), ftxui::text(" (>= 6 and <= 8 character words)")),
+				getAdjacentText(getColoredText("hard"), ftxui::text(" (>=9 character words)")),
+				ftxui::separatorEmpty(),
+				ftxui::paragraphAlignCenter("During a game session, type the onscreen words."),
+				ftxui::separatorEmpty(),
+				getAdjacentText(ftxui::text("Press "), getColoredText("ENTER"), ftxui::text(" to confirm a typed word.")),
+				getAdjacentText(ftxui::text("Press "), getColoredText("BACKSPACE"), ftxui::text(" to erase individual characters.")),
+				getAdjacentText(ftxui::text("Press "), getColoredText("CTRL + W"), ftxui::text(" to erase an entire word all at once.")),
+				getAdjacentText(ftxui::text("Press "), getColoredText("ESCAPE"), ftxui::text(" to quit the game at any time.")),
 			})
 		};
 
@@ -108,7 +120,10 @@ namespace Keywords
 				ftxui::filler(),
 				getDifficultySelection(config),
 				ftxui::filler(),
-				inputComponent.draw() | ftxui::border | ftxui::size(ftxui::WIDTH, ftxui::EQUAL, 25) | ftxui::notflex | ftxui::center,
+				inputComponent.draw() 
+				| ftxui::border 
+				| ftxui::size(ftxui::WIDTH, ftxui::EQUAL, 25) 
+				| ftxui::notflex | ftxui::center,
 				ftxui::filler(),
 							}) | ftxui::border;
 		})};
