2bbdf310a53f6c2967a35e99497888536245fd2f
Oakamoore <emmanuelogie550@yahoo.com>
2024-09-06 17:18:26 +0100

    Allow input to change the difficulty of a `Session`

---
+++ include/main_menu.h
@@ -0,0 +1,15 @@
+#ifndef KEYWORDS_MAIN_MENU_H
+#define KEYWORDS_MAIN_MENU_H
+
+#include "input_component.h"
+#include "session.h"
+
+namespace Keywords
+{
+	namespace MainMenu
+	{
+		void handleInput(SessionConfig& config, InputComponent& inputComponent);
+	}
+}
+
+#endif
--- src/main_menu.cpp
+++ src/main_menu.cpp
@@ -1,5 +1,4 @@
-#include "session.h"
-#include "input_component.h"
+#include "main_menu.h"
 #include <ftxui/component/component.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <array>
@@ -8,14 +7,9 @@
 
 namespace
 {
-	constexpr std::size_t getDifficultyCount()
-	{
-		return static_cast<std::size_t>(Keywords::SessionConfig::Difficulty::max_difficulty);
-	}
-
 	using namespace std::string_view_literals;
 
-	constexpr std::array<std::string_view, getDifficultyCount()> g_difficultyStrings
+	constexpr std::array g_difficultyOptions
 	{
 		"easy"sv,
 		"medium"sv,
@@ -24,9 +18,9 @@ namespace
 
 	std::optional<Keywords::SessionConfig::Difficulty> getDifficultyFromString(std::string_view str)
 	{
-		for (std::size_t i {0}; i < getDifficultyCount(); ++i)
+		for (std::size_t i {0}; i < g_difficultyOptions.size(); ++i)
 		{
-			if (str == g_difficultyStrings[i])
+			if (str == g_difficultyOptions[i])
 				return static_cast<Keywords::SessionConfig::Difficulty>(i);
 		}
 
@@ -59,6 +53,8 @@ namespace
 		{
 			ftxui::vbox
 			({
+				ftxui::text("DIFFICULTY:") | ftxui::bold | ftxui::center,
+				ftxui::separatorEmpty(),
 				ftxui::text(" easy ") | ftxui::inverted | ftxui::center,
 				ftxui::separatorEmpty(),
 				ftxui::text(" medium ") | ftxui::inverted | ftxui::center,
@@ -74,9 +70,24 @@ namespace
 
 namespace Keywords
 {
-	ftxui::Component getMainMenuComponent([[maybe_unused]] SessionConfig& config, InputComponent& input)
+	void MainMenu::handleInput(SessionConfig& config, InputComponent& inputComponent)
 	{
-		auto component {ftxui::Renderer(input.component, [&]
+		if (inputComponent.hasPressedEnter)
+		{
+			auto difficulty {getDifficultyFromString(inputComponent.content)};
+
+			if (difficulty)
+				config.difficulty = *difficulty;
+
+			// Clear the input component
+			inputComponent.reset();
+		}
+	}
+
+	ftxui::Component getMainMenuComponent(SessionConfig& config, 
+										  InputComponent& inputComponent)
+	{
+		auto component {ftxui::Renderer(inputComponent.component, [&]
 		{
 			return
 				ftxui::vbox({
@@ -87,11 +98,11 @@ namespace Keywords
 				ftxui::filler(),
 				getDifficultySelection(config),
 				ftxui::filler(),
-				input.draw() | ftxui::border | ftxui::size(ftxui::WIDTH, ftxui::EQUAL, 25) | ftxui::size(ftxui::HEIGHT, ftxui::EQUAL, 5) | ftxui::notflex | ftxui::center,
+				inputComponent.draw() | ftxui::border | ftxui::size(ftxui::WIDTH, ftxui::EQUAL, 25) | ftxui::notflex | ftxui::center,
 				ftxui::filler(),
 							}) | ftxui::border;
 		})};
-
+		
 		return component;
 	}
 }
